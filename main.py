a902d0962551a8bfce5c7b9e359b563c933d5b5f
yyccPhil <112043923+yyccPhil@users.noreply.github.com>
2022-11-30 20:10:36 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from cmath import exp, pi
+
+
+def dft(s_lst):
+    _n = len(s_lst)
+    for k in range(_n):
+        xk = 0
+        for s in s_lst:
+            xk += s * exp(-1j * 2*pi * k * s_lst.index(s) / _n)
+        print("X{} = {}".format(k, xk))
+    print('Note: cmath library writes complex numbers in "a+bj" form.')
+
+
+def in_operate():
+    i_lst = input('Please enter your samples (split by "," | complex numbers in "a+bi" format):\n').split(",")
+    sample_lst = list()
+    for i in i_lst:
+        if "i" in i:
+            if i.index("i") == 0 or (not i[i.index("i") - 1].isdigit()):
+                sample_lst.append(complex(i.replace(" ", "").replace("i", "1j")))
+        else:
+            sample_lst.append(int(i.replace(" ", "")))
+    return sample_lst
+
+
+if __name__ == '__main__':
+    # [0, 0, 2, 3, 4, 0, 0, 0]
+    # [1, i, -1, -i, 1, i, -1, -i]
+    # [1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0, 1, 0, -1, 0]
+
+    dft(in_operate())
+
+
